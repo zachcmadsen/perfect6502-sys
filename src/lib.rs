b48934b5600b622b6557ff3a3d883d7717fd8b0a
Zach Madsen <zachcmadsen@gmail.com>
2024-01-13 11:02:31 -0800

    Reformat hex numbers in the smoke test

--- src/lib.rs
+++ src/lib.rs
@@ -9,19 +9,19 @@ mod tests {
     use super::*;
 
     #[test]
-    fn reset_and_lda() {
+    fn smoke() {
         unsafe {
             let state = initAndResetChip();
 
             assert_eq!(readA(state), 0x00);
 
             // Write 0x0200 to the reset vector.
-            memory[0xfffc] = 0x00;
-            memory[0xfffd] = 0x02;
+            memory[0xFFFC] = 0x00;
+            memory[0xFFFD] = 0x02;
 
-            // Write lda #$ef to 0x0200.
-            memory[0x0200] = 0xa9;
-            memory[0x0201] = 0xef;
+            // Write LDA #$EF to 0x0200.
+            memory[0x0200] = 0xA9;
+            memory[0x0201] = 0xEF;
 
             // Execute the 9-cycle reset sequence.
             for _ in 0..9 {
@@ -31,13 +31,13 @@ mod tests {
 
             assert_eq!(readPC(state), 0x0200);
 
-            // Execute lda #$ef.
+            // Execute LDA #$EF.
             step(state);
             step(state);
             step(state);
             step(state);
 
-            assert_eq!(readA(state), 0xef);
+            assert_eq!(readA(state), 0xEF);
 
             destroyChip(state);
         }
