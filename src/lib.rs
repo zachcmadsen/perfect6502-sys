838e2d2f3e09c525d90d2b0f4231c7c2d8ac135f
Zach Madsen <zach@zachmadsen.com>
2024-07-12 16:34:17 -0700

    Reference more symbols in the unit test, and add more comments

--- src/lib.rs
+++ src/lib.rs
@@ -9,11 +9,10 @@ mod tests {
     use super::*;
 
     #[test]
-    fn smoke() {
+    fn lda_imm() {
         unsafe {
             let state = initAndResetChip();
-
-            assert_eq!(readA(state), 0x00);
+            assert!(!state.is_null());
 
             // Write 0x0200 to the reset vector.
             memory[0xFFFC] = 0x00;
@@ -23,21 +22,48 @@ mod tests {
             memory[0x0200] = 0xA9;
             memory[0x0201] = 0xEF;
 
+            let sp = readSP(state);
+
             // Execute the 9-cycle reset sequence.
             for _ in 0..9 {
                 step(state);
                 step(state);
             }
 
+            // The stack pointer is decremented three times during the reset
+            // sequence.
+            assert_eq!(readSP(state), sp - 3);
+
+            // PC is at the reset vector.
             assert_eq!(readPC(state), 0x0200);
 
-            // Execute LDA #$EF.
+            let x = readX(state);
+            let y = readY(state);
+
+            // Execute the first cycle of LDA #$EF.
             step(state);
             step(state);
+
+            assert_eq!(readIR(state), 0xA9);
+            assert_eq!(readRW(state), 1);
+            assert_eq!(readAddressBus(state), 0x201);
+            assert_eq!(readDataBus(state), 0xEF);
+            
+            // Execute the second cycle of LDA #$EF.
             step(state);
             step(state);
 
             assert_eq!(readA(state), 0xEF);
+
+            // X and Y are unchanged.
+            assert_eq!(readX(state), x);
+            assert_eq!(readY(state), y);
+
+            // The Zero flag is clear.
+            assert_eq!(readP(state) & 0x02, 0);
+
+            // There was 22 half cycles, i.e., we called step 22 times.
+            assert_eq!(cycle, 22);
 
             destroyChip(state);
         }
